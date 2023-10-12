47dffd5e9ee67a2abe5a0ac5a90903f001f89224
jjones <jimmy.jones@tutanota.com>
2023-10-11 18:41:12 -0600

    add sample data, draw circles on svg canvas

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import data from '$lib/data/data';
+	import { max, scaleLinear } from 'd3';
+	console.log(data);
+
+	let width = 400;
+	let height = 400;
+
+	const xScale = scaleLinear().domain([0, 100]).range([0, width]);
+	const yScale = scaleLinear()
+		.domain([0, max(data, (d: any) => d.hours)])
+		.range([height, 0]);
+</script>
+
+<svg {height} {width}>
+	{#each data as student}
+		<circle
+			cx={xScale(student.grade)}
+			cy={yScale(student.hours)}
+			r="10"
+			fill="purple"
+			stroke="black"
+		/>
+	{/each}
+</svg>
